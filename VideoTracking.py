b07218e731dffc26d0b5045b7603a56a7098303e
Titanius22 <erikparker2013@gmail.com>
2015-07-08 21:58:35 -0400

    Change camera functions

--- VideoTracking.py
+++ VideoTracking.py
@@ -28,76 +28,78 @@ Red 160-180
 """
 
 # initialize the camera and grab a referance to the raw camera capture
-camera = PiCamera()
-camera.resolution = (1920, 1080) ##########Could maybe change later
-rawCapture = PiRGBArray(camera)
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as stream:
+        camera.resolution = (1920, 1080)
+        camera.capture(rawCapture, 'bgr', use_video_port=True)     
 
-# allow camera to warm up
-time.sleep(0.1)
+        # take first frame of the video
+        camera.capture(rawCapture, format="bgr")
+        frame = rawCapture.array
+        
+        cv2.imshow('frame', frame)
 
-# take first frame of the video
-camera.capture(rawCapture, format="bgr")
-frame = rawCapture.array
+        # setup initial location of window
+        r,h,c,w = 250,90,400,125  # simply hardcoded the values
+        track_window = (c,r,w,h)
 
-# setup initial location of window
-r,h,c,w = 250,90,400,125  # simply hardcoded the values
-track_window = (c,r,w,h)
+        # set up the ROI for tracking
+        roi = frame[r:r+h, c:c+w] #create smaller frame
+        hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #convert to HSV
 
-# set up the ROI for tracking
-roi = frame[r:r+h, c:c+w] #create smaller frame
-hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #convert to HSV
+        mask = cv2.inRange(hsv_roi, lower_hue, upper_hue) #np.array((0., 60.,32.)), np.array((180.,255.,255.))
+        roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180]) #([hsv_roi],[0],mask,[180],[0,180])
+        cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
 
-mask = cv2.inRange(hsv_roi, lower_hue, upper_hue) #np.array((0., 60.,32.)), np.array((180.,255.,255.))
-roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180]) #([hsv_roi],[0],mask,[180],[0,180])
-cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(frame,frame, mask= mask)
 
-# Bitwise-AND mask and original image
-res = cv2.bitwise_and(frame,frame, mask= mask)
+        # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+        term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
-# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-
-while(True):
+        while True:
     
-    # empty image buffer
-    rawCapture.truncate(0) 
+            # empty image buffer
+            rawCapture.seek(0) 
+            rawCapture.truncate()
+            
     
-    # capture and retrieve frame
-    camera.capture(rawCapture, format="bgr")
-    #camera.capture(rawCapture, use_video_port=True, format="bgr")
-    frame = rawCapture.array
+            # capture and retrieve frame
+            camera.capture(rawCapture, 'bgr', use_video_port=True)
+            #camera.capture(rawCapture, use_video_port=True, format="bgr")
+            frame = rawCapture.array
 
-    if frame.size: #if array has number then its true
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+            if frame.size: #if array has number then its true
+                hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
         
-        #preShift = track_window
+                #preShift = track_window
 
-        # Apply meanshift to get the new location
-        ret, track_window = cv2.meanShift(dst, track_window, term_crit)
-        if ret != True:
-            print "ret is not True"
-            break
+                # Apply meanshift to get the new location
+                ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+                if ret != True:
+                    print "ret is not True"
+                    break
         
-        # The change  X and Y 
-        #deltaPos = cv2.subtract((track_window[0],track_window[1]), (preShift[0], preShift[1]))
+                # The change  X and Y 
+                #deltaPos = cv2.subtract((track_window[0],track_window[1]), (preShift[0], preShift[1]))
 
-        # Draw it on image
-        x,y,w,h = track_window
-        img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+                # Draw it on image
+                x,y,w,h = track_window
+                img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
         
-        cv2.imshow('img2',img2)
+                cv2.imshow('img2',img2)
 
-        k = cv2.waitKey(60) & 0xff
-        if k == 27:
-            print "Escape key was pressed"
-            break
-        else:
-            cv2.imwrite(chr(k)+".jpg",img2)
+                k = cv2.waitKey(60) & 0xff
+                if k == 27:
+                    print "Escape key was pressed"
+                    break
+                else:
+                    cv2.imwrite(chr(k)+".jpg",img2)
 
-    else:
-        print "Frame array was empty"
-        break
+            else:
+                print "Frame array was empty"
+                break
 
-cv2.destroyAllWindows()
-camera.close()
+        cv2.destroyAllWindows()
+        camera.close()
