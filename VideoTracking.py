dba3c5a71489692a98699b709e5641125a029ea6
Titanius22 <erikparker2013@gmail.com>
2015-07-08 21:36:41 -0400

    Added test.py to test cv2.imshow

--- VideoTracking.py
+++ VideoTracking.py
@@ -71,7 +71,7 @@ while(True):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
         
-        preShift = track_window
+        #preShift = track_window
 
         # Apply meanshift to get the new location
         ret, track_window = cv2.meanShift(dst, track_window, term_crit)
