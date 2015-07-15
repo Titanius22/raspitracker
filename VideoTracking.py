6748ae7a8918fe6af749c3f59852de223e9be81b
Titanius22 <erikparker2013@gmail.com>
2015-07-15 00:03:48 -0400

    Added all I learned from PC code

--- VideoTracking.py
+++ VideoTracking.py
@@ -1,13 +1,24 @@
+"""
+TODO
+-make function to tell servos to move 
+-make option to switch to face tracking
+-make the servo's shake when track is reset so the user knows to pull the object back
+
+
+
+"""
 import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-# from matplotlib import pyplot as plt
 
 # wierd thing required for cv2 to work
 import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
+
+# How ofter will it check for 'reset'
+resetCounter = 5
 
 #Resolution
 camWidth = 320
@@ -36,6 +47,36 @@ Purple 140-155
 Red 160-180
 """
 
+def Calibrate_NewObject():
+    
+    # Make servo shake
+    
+    # Give time for object to back away
+    time.sleep(.4)
+    
+    # empty image buffer
+    rawCapture.seek(0) 
+    rawCapture.truncate()
+    
+    # campure a frame of the video
+    camera.capture(rawCapture, 'bgr', use_video_port=True)     
+    frame = rawCapture.array
+
+    # setup initial location of window
+    r,h,c,w = int(.5*(camHeight-trackHeight)), trackHeight, int(.5*(camWidth-trackWidth)), trackWidth  # Defined above. used int() to keep type integer
+    track_window = (c,r,w,h)
+
+    # set up the ROI for tracking
+    roi = frame[r:r+h, c:c+w] #creat smaller frame
+    hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV) #convert to HSV
+
+    mask = cv2.inRange(hsv_roi, lower_hue, upper_hue) #np.array((0., 60.,32.)), np.array((180.,255.,255.))
+    roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180]) #([hsv_roi],[0],mask,[180],[0,180])
+    cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+    
+    return track_window, roi_hist
+
+
 # initialize the camera and grab a referance to the raw camera capture
 with PiCamera() as camera:
     with PiRGBArray(camera) as rawCapture:
@@ -46,7 +87,7 @@ with PiCamera() as camera:
         frame = rawCapture.array
 
         # setup initial location of window
-        r,h,c,w = (.5*(camHeight-trackHeight)), trackHeight, (.5*(camWidth-trackWidth)), trackWidth  # Defined above
+        r,h,c,w = int(.5*(camHeight-trackHeight)), trackHeight, int(.5*(camWidth-trackWidth)), trackWidth  # Defined above. used int() to keep type integer
         track_window = (c,r,w,h)
 
         # set up the ROI for tracking
@@ -69,26 +110,22 @@ with PiCamera() as camera:
             rawCapture.seek(0) 
             rawCapture.truncate()
             
-    
             # capture and retrieve frame
             camera.capture(rawCapture, 'bgr', use_video_port=True)
-            #camera.capture(rawCapture, use_video_port=True, format="bgr")
             frame = rawCapture.array
 
-            if frame.size: #if array has number then its true
+            if frame.size: #if array has numbers then its true
                 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
                 dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
         
-                #preShift = track_window
+                preShift = track_window
 
                 # Apply meanshift to get the new location
                 ret, track_window = cv2.meanShift(dst, track_window, term_crit)
-                if ret != True:
-                    print "ret is not True"
-                    break
         
-                # The change  X and Y 
-                #deltaPos = cv2.subtract((track_window[0],track_window[1]), (preShift[0], preShift[1]))
+                # The change  X and Y. posative isup and to the right
+                deltaPos = cv2.subtract((track_window[0],track_window[1]), (preShift[0], preShift[1]))
+                print deltaPos
 
                 # Draw it on image
                 x,y,w,h = track_window
@@ -100,8 +137,16 @@ with PiCamera() as camera:
                 if k == 27:
                     print "Escape key was pressed"
                     break
-                else:
-                    cv2.imwrite(chr(k)+".jpg",img2)
+                    
+                # Used as a 'reset' button to start tracking new object TODO
+                resetCounter = resetCounter -1
+                if resetCounter == 0:
+                    test = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    frameMean = int((cv2.meanStdDev(test))[0])
+                    frameStdDev = int((cv2.meanStdDev(test))[1])
+                    if frameStdDev < 10:
+                        track_window, roi_hist = Calibrate_NewObject()
+                        resetCounter = 5
 
             else:
                 print "Frame array was empty"
